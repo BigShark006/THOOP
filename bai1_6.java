4f867c00f202e0d1cef84b8e5cadea1e397b99b2
BigShark <nguyenvohoangphi.2006@gmail.com>
2026-02-07 08:37:24 +0700

    Update bai1_6.java

--- bai1_6.java
+++ bai1_6.java
@@ -1,43 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 import java.util.Scanner;
 
-public class bai1_6 {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap he so a: ");
+
         double a = scanner.nextDouble();
-        System.out.print("Nhap he so b: ");
         double b = scanner.nextDouble();
-        System.out.print("Nhap he so c: ");
         double c = scanner.nextDouble();
 
         if (a == 0) {
             if (b == 0) {
                 if (c == 0) {
-                    System.out.println("Phuong trinh vo so nghiem.");
+                    System.out.println("Countless");
                 } else {
-                    System.out.println("Phuong trinh vo nghiem.");
+                    System.out.println("None");
                 }
             } else {
                 double x = -c / b;
-                System.out.println("Phuong trinh co mot nghiem: x = " + x);
+                System.out.printf("%.2f " , x);
             }
         } else {
             double delta = b * b - 4 * a * c;
             if (delta < 0) {
-                System.out.println("Phuong trinh vo nghiem.");
+                System.out.println("None");
             } else if (delta == 0) {
                 double x = -b / (2 * a);
-                System.out.println("Phuong trinh co nghiem kep: x1 = x2 = " + x);
+                System.out.printf("%.2f " , x);
             } else {
                 double x1 = (-b + Math.sqrt(delta)) / (2 * a);
                 double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-                System.out.println("Phuong trinh co hai nghiem phan biet:");
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
+                System.out.printf("%.2f " , x1);
+                System.out.printf("%.2f " , x2);
             }
         }
         scanner.close();
     }
 }
 
-//Giai phuong trinh bac hai ax^2 + bx + c = 0
